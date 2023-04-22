d3228a6e48583a2f8f0c3b040c9c0ad957b3a73c
AlexandrTarabanko <clarityabovezero@gmail.com>
2023-04-22 22:24:37 +0300

    Finished

--- src/helpers/fetchCountries.js
+++ src/helpers/fetchCountries.js
@@ -2,17 +2,14 @@ const BASE_URL = 'https://restcountries.com/v3.1';
 const END_POINT = '/name/';
 
 function fetchCountries(name) {
-  return (
-    fetch(`${BASE_URL}${END_POINT}${name}`)
-      .then(res => {
-        if (!res.ok) {
-          throw new Error(res.status);
-        }
-        return res.json();
-      })
-      // .then(data => console.log(data))
-      .catch(err => console.log(err))
-  );
+  return fetch(`${BASE_URL}${END_POINT}${name}`)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+      return res.json();
+    })
+    .catch(err => console.log(err));
 }
 
 export { fetchCountries };
